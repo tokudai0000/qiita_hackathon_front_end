149f1696218173b424628da5131a48db51c07919
akidon0000 <akidon0000@gmail.com>
2024-02-10 17:47:00 +0900

    Entityは別ブランチで作る

--- App/qiita_hackathon/Entity/ProgramLang.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ProgramLang.swift
-//  qiita_hackathon
-//
-//  Created by Akihiro Matsuyama on 2024/02/10.
-//
-
-struct ProgramLang: Decodable {
-}
--- App/qiita_hackathon/Entity/SnsLink.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SnsLink.swift
-//  qiita_hackathon
-//
-//  Created by Akihiro Matsuyama on 2024/02/10.
-//
-
-struct SnsLink: Decodable {
-}
