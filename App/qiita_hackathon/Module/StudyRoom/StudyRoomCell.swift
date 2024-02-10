88d3893020b2fedacefde74b1ab80e8a7cca93be
hinakko <131275914+hinakko02@users.noreply.github.com>
2024-02-10 19:21:44 +0900

    StudyRoomCell作成

--- App/qiita_hackathon/Module/StudyRoom/StudyRoomCell.swift
+++ App/qiita_hackathon/Module/StudyRoom/StudyRoomCell.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class StudyRoomCell: UICollectionViewCell {
 
+    @IBOutlet weak var userImage: UIImageView!
+
+    @IBOutlet weak var drinkLabel: UILabel!
+
+    @IBOutlet weak var langLabel: UILabel!
+
+    @IBOutlet weak var elapsedTime: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    func setUp(with sample: SampleData) {
+        userImage.image = sample.image
+        drinkLabel.text = sample.drink
+        langLabel.text = sample.lang
+        elapsedTime.text = sample.elapsedTime
 
+
+
+    }
 }
