cf93f4043fe855b8788e1e91109b07aa5ea527ae
akidon0000 <akidon0000@gmail.com>
2024-02-10 22:43:41 +0900

    AccountSettingsを簡単に実装

--- App/qiita_hackathon/Module/AccountSettings/AccountSettingsViewController.swift
+++ App/qiita_hackathon/Module/AccountSettings/AccountSettingsViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class AccountSettingsViewController: UIViewController {
-    @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var iconImageView: UIImageView!
 
     var userData: User? = Common().sampleUserData
@@ -22,10 +21,6 @@ class AccountSettingsViewController: UIViewController {
     }
 
     private func configStackView() {
-        stackView.axis = .vertical
-        stackView.distribution = .equalSpacing
-        stackView.alignment = .center
-        stackView.spacing = 10
     }
 
     private func configIconImageView() {
@@ -34,26 +29,5 @@ class AccountSettingsViewController: UIViewController {
     }
 
     private func configNameField() {
-        let firstView = UIView()
-        firstView.backgroundColor = .red
-
-        let secondView = UIView()
-        secondView.backgroundColor = .blue
-
-        let thirdView = UIView()
-        thirdView.backgroundColor = .green
-
-        // 各ビューをstackViewに追加
-        view.addSubview(firstView)
-        stackView.addArrangedSubview(secondView)
-        stackView.addArrangedSubview(thirdView)
-
-        let inputView = InputFieldView()
-        stackView.addArrangedSubview(inputView)
-        inputView.setTitleText("iou")
-
-        let inputView2 = InputFieldView()
-        inputView2.setTitleText("iou")
-        stackView.addArrangedSubview(inputView2)
     }
 }
