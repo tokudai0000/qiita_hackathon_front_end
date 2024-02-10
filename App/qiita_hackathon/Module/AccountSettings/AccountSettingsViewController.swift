06cf170dc1b8ab4ec27a88788d4e2e1d080c5c5e
akidon0000 <akidon0000@gmail.com>
2024-02-10 15:04:40 +0900

    iconImageViewの作成

--- App/qiita_hackathon/Module/AccountSettings/AccountSettingsViewController.swift
+++ App/qiita_hackathon/Module/AccountSettings/AccountSettingsViewController.swift
@@ -8,22 +8,23 @@
 import UIKit
 
 class AccountSettingsViewController: UIViewController {
-
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var iconImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configStackView()
+        configIconImageView()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configStackView() {
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
     }
-    */
 
+    private func configIconImageView() {
+        iconImageView.image = UIImage(systemName: "eraser")
+    }
 }
