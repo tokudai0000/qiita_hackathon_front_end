52138f7b7281c898e533ffdd16fef36189ba660b
akidon0000 <akidon0000@gmail.com>
2024-02-10 23:07:06 +0900

    AccountSettingsの必要機能追加

--- App/qiita_hackathon/Module/AccountSettings/AccountSettingsViewController.swift
+++ App/qiita_hackathon/Module/AccountSettings/AccountSettingsViewController.swift
@@ -9,25 +9,49 @@ import UIKit
 
 class AccountSettingsViewController: UIViewController {
     @IBOutlet weak var iconImageView: UIImageView!
-
+    // TODO: 再利用できる
+    @IBOutlet weak var nameTextFielld: UITextField!
+    @IBOutlet weak var companionDrinkButton: UIButton!
+    @IBOutlet weak var langButton: UIButton!
+    @IBOutlet weak var sns1: UIStackView!
+    @IBOutlet weak var sns2: UITextField!
+    
     var userData: User? = Common().sampleUserData
+
+    enum DisplayType {
+        case settings
+        case display
+    }
+    var displayType: DisplayType = .settings
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        configStackView()
         configIconImageView()
-        configNameField()
+        configNameTextFielld()
+        configCompanionDrinkButton()
+        configLangButton()
+        configSns()
     }
 
-    private func configStackView() {
-    }
 
     private func configIconImageView() {
         iconImageView.image = UIImage(systemName: "eraser")
         iconImageView.backgroundColor = .red
     }
 
-    private func configNameField() {
+    private func configNameTextFielld() {
+        nameTextFielld.text = userData?.userName
+    }
+
+    private func configCompanionDrinkButton() {
+    }
+
+    private func configLangButton() {
+    }
+
+    private func configSns() {
+//        sns1.text = userData?.sns1
+//        sns2.text = userData?.sns2
     }
 }
