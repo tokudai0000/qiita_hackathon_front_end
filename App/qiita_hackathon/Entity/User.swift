dbfcc7162c75b278ffeb75d24b24cac5c71992d8
akidon0000 <akidon0000@gmail.com>
2024-02-10 19:01:22 +0900

    entryTimeとlangを追加

--- App/qiita_hackathon/Entity/User.swift
+++ App/qiita_hackathon/Entity/User.swift
@@ -5,6 +5,8 @@
 //  Created by Akihiro Matsuyama on 2024/02/10.
 //
 
+import Foundation
+
 struct User: Decodable {
     let id: String
     let iconImage: String
@@ -12,14 +14,18 @@ struct User: Decodable {
     let companionDrink: Int
     let programLangs: [ProgramLang]
     let snsLinks: [SnsLink]
+    let entryTime: String
+    let lang: Int
 
-    init(id: String, iconImage: String, userName: String, companionDrink: Int, programLangs: [ProgramLang], snsLinks: [SnsLink]) {
+    init(id: String, iconImage: String, userName: String, companionDrink: Int, programLangs: [ProgramLang], snsLinks: [SnsLink], entryTime: String, lang: Int) {
         self.id = id
         self.iconImage = iconImage
         self.userName = userName
         self.companionDrink = companionDrink
         self.programLangs = programLangs
         self.snsLinks = snsLinks
+        self.entryTime = entryTime
+        self.lang = lang
     }
 
     enum ParsingError: Error {
@@ -32,7 +38,9 @@ struct User: Decodable {
               let userName = dictionary["userName"] as? String,
               let companionDrink = dictionary["companionDrink"] as? Int,
               let programLangs = dictionary["programLangs"] as? [ProgramLang],
-              let snsLinks = dictionary["snsLinks"] as? [SnsLink] else {
+              let snsLinks = dictionary["snsLinks"] as? [SnsLink],
+              let entryTime = dictionary["entryTime"] as? String,
+              let lang = dictionary["lang"] as? Int else {
             throw ParsingError.invalidData
         }
         self.id = id
@@ -41,5 +49,7 @@ struct User: Decodable {
         self.companionDrink = companionDrink
         self.programLangs = programLangs
         self.snsLinks = snsLinks
+        self.entryTime = entryTime
+        self.lang = lang
     }
 }
